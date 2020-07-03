c2a98e1c80be36063258ad6638b460ec8e1fd566
FrostPriest <johnericjahn@gmail.com>
2020-07-03 14:40:54 -0300

    Register Page

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
+import { Form } from '@unform/web';
+
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content, Background } from './styles';
+
+const SignUp: React.FC = () => {
+  function handleSubmit(data: any): void {
+    console.log(data);
+  }
+
+  return (
+    <Container>
+      <Background />
+
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu logon</h1>
+          <Input placeholder="Nome" name="name" icon={FiUser} />
+          <Input placeholder="E-mail" name="email" icon={FiMail} />
+          <Input placeholder="Senha" name="password" icon={FiLock} />
+
+          <Button type="submit">Entrar</Button>
+        </Form>
+
+        <a href="login">
+          <FiArrowLeft />
+          Voltar para logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
+
+export default SignUp;
